c188e29f710b357fe4ab6bc3f69fa50468bd1c9f
Nourhan Muhammad <113142492+nouraa17@users.noreply.github.com>
2023-12-02 22:57:13 +0200

    Update FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,19 +11,10 @@ class FormController extends Controller
         // Retrieve data from the request
         $email = $request->input('email');
         $password = $request->input('pwd');
-        $remember = $request->has('remember'); // Checkbox value
-
-        // Now you can do something with the data, e.g., store it in the database
+        $remember = $request->has('remember'); 
+        
         echo "Email: $email<br>";
         echo "Password: $password<br>";
         echo "Remember: " . ($remember ? 'Yes' : 'No') . "<br>";
-        // exit;
-        // dd([
-        //     'email' => $email,
-        //     'password' => $password,
-        //     'remember' => $remember,
-        // ]);
-        // Return a response or redirect
-        // return redirect()->route('logged'); // Replace 'some.route' with your desired route
     }
 }
